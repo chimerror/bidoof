dc84847fa14182bb830c0e53e9a50c374964f293
Jayce Mitchell <chimerror@live.com>
2021-05-02 21:47:30 -0700

    Pulling in play-cljc template

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject bidoof "0.1.0-SNAPSHOT"
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :sign-releases false}]]
+  :clean-targets ^{:protect false} ["target"]
+  :main bidoof.start
+  :aot [bidoof.start])
---
+++ src/bidoof/start_dev.cljs
@@ -0,0 +1,6 @@
+(ns bidoof.start-dev
+  (:require [bidoof.start]
+            [clojure.spec.test.alpha :as st]))
+
+(st/instrument)
+
